53e81a0d37abbbed3095d97a9f0d62b410db0fca
jrdev0 <104781534+jrdev0@users.noreply.github.com>
2022-05-11 23:31:13 +0100

    Add files via upload

---
+++ co_znaczy_slang.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def pobranie_strony(co_to):
+
+    headers = {'User_Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0'}
+    wyszukaj = f'https://www.miejski.pl/slowo-{co_to}'
+    r = requests.get(wyszukaj, headers)
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    return soup
+
+
+def slang(soup):
+    lista = []
+    opis = soup.find_all('main')
+    for op in opis:
+        # MOZE DO UZYTKU W POZNIEJSZYM CZASIE
+
+        slowo = op.find('h1').text
+    #     try:
+    #         ocena = str(op.find('span', class_='rating').text).replace('  ', '').replace("'", "")
+    #     except AttributeError:
+    #         ocena = ''
+        rezultat = str(op.p.text).replace('  ', '').replace('\r\n', '').replace('\n', '').replace("'", "")
+        try:
+            przyklad = str(op.find('blockquote').text).replace('\r\n', '').replace('\n', '').replace('  ', '').replace('!2', '. Przykład 2').replace('"', '').replace('***', '')
+        except AttributeError:
+            przyklad = ''
+        if 'której szukasz' in rezultat:
+            lol = str({rezultat})
+            lol = lol.replace('{', ' ').replace("}", ' ').replace('[', '').replace("[", "").replace('Strona, której szukasz nie istnieje', 'Brak znaczenia tego słowa bazie danych')
+        else:
+            lol = {
+                ' ': slowo,
+                '': rezultat,
+                'przykład ': przyklad
+
+            }
+        lista.append(lol)
+    list2 = str(lista).replace("{", "").replace("}", "").replace("'", "")
+
+    return list2
+
+
+def slang_wykonanie(slowo):
+
+    x = pobranie_strony(slowo)
+    rezult = slang(x)
+
+    return str(rezult)
